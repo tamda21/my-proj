f41e7f8daf54370a8f8e0e2d7479b7bc67bae6e3
Tamda <abdetam452@gmail.com>
2021-08-20 01:33:49 +0200

    Create Help in header

---
+++ src/views/Help.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="help">
+        <h1>first header</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+            consectetur nulla corporis? Magnam, mollitia deleniti laborum
+            officiis nisi autem sapiente?
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "help",
+};
+</script>
