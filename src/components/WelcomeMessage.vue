3eaae520b909d244d27bf197bca80526af772424
Tamda <abdetam452@gmail.com>
2021-08-19 15:23:49 +0200

    Create Propr Page

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="our">
+        <h2>hello world</h2>
+        <p>this is paragragh for exmple</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WelcomeMessage",
+};
+</script>
+
+<style scoped lang="scss">
+$mainSize: 40px;
+h2 {
+    font-size: $mainSize;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <WelcomeMessage />
+    </div>
 </template>
+<script>
+import WelcomeMessage from "@/components/WelcomeMessage.vue";
+
+export default {
+    name: " About",
+    components: {
+        WelcomeMessage,
+    },
+};
+</script>
